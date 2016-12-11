551ad23f09cf3f5c01f0af4c179db2c1d0f40d6a
shubhangihora <shubsy9@gmail.com>
2016-12-12 00:58:05 +0530

    updating the js file

--- tasks.js
+++ tasks.js
@@ -1,19 +1,21 @@
 new Vue ({
-  el: '#task-form',
+  el: '#app',
   data:
   {
+    task: { taskName: '', taskDesc: '', deadline: ''},
+
     tasks: [
-    {
-      taskName: '', taskDesc: '', deadline: ''
-    },
-    {
-      taskName: 'Software Engg', taskDesc: 'Final Eval Component', deadline: '11.12.2016'
-    }
+      {taskName: "submit git code", taskDesc: "git push test for Final Eval Component", deadline: "11.12.2016"},
+        {taskName: "Do the web page", taskDesc: "create the design for web page", deadline: "11.12.2016"}
   ]},
+
   methods:
   {
     addTask: function(){
+    //  alert("hello " + this.task.taskName + " " + this.task.taskDesc + " " + this.task.deadline);
       this.tasks.push(this.task);
+      this.task = { taskName: '', taskDesc: '', deadline: ''};
+
     }
   }
 
